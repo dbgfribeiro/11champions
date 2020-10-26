01ad299779dbd6edc7d183acfa076a8e0db0760e
dbgfribeiro <dribeiro641@gmail.com>
2020-10-26 11:42:45 +0000

    top scorers & leaderboards created

---
+++ leaderboards.php
@@ -0,0 +1,72 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/leaderboards.css">
+    <title>11Champions</title>
+</head>
+<body>
+<header>
+    <nav>
+        <div class="nav-menu">
+            <a href="index.html" id="logo">1<span>1</a>
+            <div class="menu-desktop">
+                <ul>
+                    <a href="leaderboards.php"><li>CLASSIFICAÇÃO</li></a>
+                    <a href="/"><li>ÚLTIMOS RESULTADOS</li></a>
+                    <a href="/"><li>CALENDÁRIO</li></a>
+                    <a href="scorers.php"><li>MARCADORES</li></a>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
+<main>
+    <div class="container">
+        <h1>CLASSIFICAÇÕES</h1>
+
+        <?php
+        echo "
+                <table>
+                    <tr>
+                        <th>#</th>
+                        <th>Equipa</th>
+                        <th>P</th>
+                        <th>NºJ</th>
+                        <th>V</th>
+                        <th>E</th>
+                        <th>D</th>
+                        <th>GM</th>
+                        <th>GS</th>
+                    </tr>";
+
+        $str = "dbname=11test user=postgres password=postgres host=localhost port=5432";
+        $conn = pg_connect($str);
+        $result = pg_query($conn, " SELECT  *  FROM team ORDER BY points DESC;");
+        $rowCount = pg_fetch_result($result, 0, 0);
+
+        while ($row = pg_fetch_assoc($result) ){
+            echo   "<tr>
+                       <td>".$rowCount++."</td>
+                       <td>".$row['team_name']."</td>
+                       <td>".$row['points']."</td>
+                       <td>".$row['n_games']."</td>
+                       <td>".$row['win']."</td>
+                       <td>".$row['lose']."</td>
+                       <td>".$row['draw']."</td>
+                       <td>".$row['g_scored']."</td>
+                       <td>".$row['g_conceded']."</td>
+                    </tr>";
+        }
+        echo  "</table>";
+        ?>
+    </div>
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
--- scorers.php
+++ scorers.php
@@ -4,56 +4,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
-    <title>11 Eleven</title>
+    <link rel="stylesheet" href="css/scorers.css">
+    <title>11Champions</title>
 </head>
 <body>
 <header>
     <nav>
         <div class="nav-menu">
             <a href="index.html" id="logo">1<span>1</a>
-            <ul>
-                <a href="/"><li>CLASSIFICAÇÃO</li></a>
-                <a href="/"><li>ÚLTIMOS RESULTADOS</li></a>
-                <a href="/"><li>CALENDÁRIO</li></a>
-                <a href="/"><li>MARCADORES</li></a>
-            </ul>
+            <div class="menu-desktop">
+                <ul>
+                    <a href="leaderboards.php"><li>CLASSIFICAÇÃO</li></a>
+                    <a href="/"><li>ÚLTIMOS RESULTADOS</li></a>
+                    <a href="/"><li>CALENDÁRIO</li></a>
+                    <a href="scorers.php"><li>MARCADORES</li></a>
+                </ul>
+            </div>
         </div>
-
-        <div class="container">
-
-                <?php
-                echo "
-                <table>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>Posição</th>
-                        <th>País</th>
-                        <th>Nº de golos</th>
-                        <th>Equipa</th>
-                    </tr>";
-
-                $str = "dbname=11test user=postgres password=postgres host=localhost port=5432";
-                $conn = pg_connect($str);
-                $result = pg_query($conn, " SELECT  *  FROM player;");
-                while ($row = pg_fetch_assoc($result) ){
-                echo   "<tr>
-                            <td>".$row['id_player']."</td>
-                            <td>".$row['name']."</td>
-                            <td>".$row['position']."</td>
-                            <td>".$row['country']."</td>
-                            <td>".$row['goals']."</td>
-                            <td>".$row['team']."</td>
-                        </tr>";
-                }
-                echo  "</table>";
-                ?>
-        </div>
-
     </nav>
 </header>
-<main>
 
+<main>
+    <div class="container">
+        <h1>MARCADORES</h1>
+
+        <?php
+
+        $str = "dbname=11test user=postgres password=postgres host=localhost port=5432";
+        $conn = pg_connect($str);
+        $result = pg_query($conn, " SELECT  *  FROM player;");
+
+
+        while ($row = pg_fetch_assoc($result) ){
+            echo
+                "
+                 <div class='player-stats'>
+                    <div class='player-info'>
+                        <h2>".$row['name']."</h2>
+                        <h4>".$row['team']."</h4>
+                        <p>".$row['position']."</p>
+                    </div>
+                    <div class='player-goals'>
+                        <h1>".$row['goals']."</h1>
+                    </div>
+                 </div>
+                "
+            ;
+        }
+        ?>
+    </div>
 </main>
 <footer>
 
